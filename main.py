393bb3cbdd99973433a6baaeb5618d91ed3513ba
Tobias Becker <24841210+Fork92@users.noreply.github.com>
2025-10-06 22:18:16 +0200

    finish project

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 from stats import get_num_words
 from stats import count_character
+from stats import sort_dict
 
 
 def get_book_text(path):
@@ -8,11 +10,23 @@ def get_book_text(path):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    text = get_book_text(sys.argv[1])
+    num_words = get_num_words(text)
+    sorted_count = sort_dict(count_character(text))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(count_character(text))
+    print("--------- Character Count -------")
+    for entry in sorted_count:
+        c = entry["char"]
+        count = entry["num"]
+        print(f"{c}: {count}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -18,3 +18,17 @@ def count_character(text):
             caracter_dic[c] = 1
 
     return caracter_dic
+
+
+def sort_dict(character_dic):
+    char_list = []
+    for c in character_dic:
+        char_dic = {"char": c, "num": character_dic[c]}
+        char_list.append(char_dic)
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+
+def sort_on(items):
+    return items["num"]
